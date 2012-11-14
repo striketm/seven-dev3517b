4ec318ac81f75128ec47b35ab71c62176fbb4511
cleber@tavaresjunior.com <cleber@tavaresjunior.com@d2ee3d9f-b597-5406-cbf2-4c2b88b0a7b6>
2012-11-14 14:37:59 +0000

    textura

--- trunk_xna/Primitiva3D/Primitiva3D/Primitiva3D/Game1.cs
+++ trunk_xna/Primitiva3D/Primitiva3D/Primitiva3D/Game1.cs
@@ -20,10 +20,15 @@ namespace Primitiva3D
         SpriteBatch spriteBatch;
 
         //Define um array que vai armazenar os vertices de uma primitiva (triangulo)
-        VertexPositionColor[] primitivas;
+        //VertexPositionColor[] primitivas;
+        
+        //Aqui nenhuma novidade, vamos apenas acrescentar as texturas:
+        VertexPositionColorTexture[] primitivas;
 
         //Cria a instância de um shader (efeito especial) que iremos aplicar na nossa primitiva
         BasicEffect efeitoBasico;
+
+        Matrix worldTest;
 
         public Game1()
         {
@@ -54,24 +59,60 @@ namespace Primitiva3D
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             //Alocando o objeto na memória 
-            primitivas = new VertexPositionColor[3];
+            //primitivas = new VertexPositionColorTexture[3];
 
-            primitivas[0] = new VertexPositionColor();
-            primitivas[0].Position = new Vector3(0, 1, 0);
+            //para desenharmos um quadrado, precisamos de dois triângulos...
+            //não podemos usar 4 pontos através do strip por causa das normais...
+            //logo:
+
+            primitivas = new VertexPositionColorTexture[6];
+
+            //ajustando as posições do primeiro triângulo para fazer a parte de cima do quadrado:
+            //lembrando a ordem e a normal
+            //adicionando o posicionamento da textura
+            primitivas[0] = new VertexPositionColorTexture();
+            primitivas[0].Position = new Vector3(-0.5f, 0.5f, 0);
             primitivas[0].Color = Color.Red;
+            
+            primitivas[0].TextureCoordinate = new Vector2(0.0f, 0.0f);
 
-            primitivas[1] = new VertexPositionColor();
-            primitivas[1].Position = new Vector3(1, -1, 0);
+            primitivas[1] = new VertexPositionColorTexture();
+            primitivas[1].Position = new Vector3(0.5f, 0.5f, 0);
             primitivas[1].Color = Color.Green;
+            
+            primitivas[1].TextureCoordinate = new Vector2(1.0f, 0.0f);
 
-            primitivas[2] = new VertexPositionColor();
-            primitivas[2].Position = new Vector3(-1, -1, 0);
+            primitivas[2] = new VertexPositionColorTexture();
+            primitivas[2].Position = new Vector3(0.5f, -0.5f, 0);
             primitivas[2].Color = Color.Blue;
 
+            primitivas[2].TextureCoordinate = new Vector2(1.0f, 1.0f);
+
+            //criando o triângulo da parte de baixo do quadrado:
+            //lembrando a ordem e a normal
+            //haverão dois pontos coincidentes
+            //mudando as cores
+
+            primitivas[3] = new VertexPositionColorTexture();
+            primitivas[3].Position = new Vector3(-0.5f, 0.5f, 0);
+            primitivas[3].Color = Color.Black;
+
+            primitivas[3].TextureCoordinate = new Vector2(0.0f, 0.0f);
+
+            primitivas[4] = new VertexPositionColorTexture();
+            primitivas[4].Position = new Vector3(0.5f, -0.5f, 0);
+            primitivas[4].Color = Color.White;
+
+            primitivas[4].TextureCoordinate = new Vector2(1.0f, 1.0f);
+
+            primitivas[5] = new VertexPositionColorTexture();
+            primitivas[5].Position = new Vector3(-0.5f, -0.5f, 0);
+            primitivas[5].Color = Color.Yellow;
+
+            primitivas[5].TextureCoordinate = new Vector2(0.0f, 1.0f);
+
             efeitoBasico = new BasicEffect(GraphicsDevice);
-
             
-
             //Cria a Matriz de visão (aqui definimos a posicao da camera (primeiro argumento), a posição alvo na qual na qual ela vai estar vendo (segundo argumento) e o Vector.Up (argumento padrão))
             efeitoBasico.View = Matrix.CreateLookAt(new Vector3(0, 0, 3), new Vector3(0, 0, 0), Vector3.Up);
 
@@ -84,13 +125,35 @@ namespace Primitiva3D
             
             efeitoBasico.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4,
             graphics.GraphicsDevice.Viewport.AspectRatio, 0.1f, 100.0f);
-
-
+            
             //Cria a Matriz de Mundo do objeto 3D (aqui definimos seu comportamento (translação/rotação/escala)) (atribuindo uma matrix identidade)
             efeitoBasico.World = Matrix.Identity;
 
-            //Habilita o efeito de cores nos vertices
-            efeitoBasico.VertexColorEnabled = true;
+            //Habilita o efeito de cores nos vértices:
+            //efeitoBasico.VertexColorEnabled = true;
+
+            //Habilita as texturas nos vértices:
+            efeitoBasico.TextureEnabled = true;
+
+            //Tem que carregar a textura:
+            efeitoBasico.Texture = Content.Load<Texture2D>("box");
+
+            //Exercício 01: Criar uma classe que crie uma primitiva com textura, como mostrado...
+
+            //Exercício 02: Criar uma classe Camera
+
+            //Exercício 03: Usando a classe que cria a "parede" acima, crie uma caixa
+            //DICA: você pode rotacionar e transladar um objeto com mais facilidade,
+            //adicionando nele uma Matrix World e chamando os métodos CreateTranslation
+            //e CreateRotation_
+
+            //
+
+            worldTest = Matrix.Identity;
+
+            worldTest = Matrix.CreateTranslation(1.0f, 0.0f, 0.0f);
+
+            //CUIDADO!
 
             // TODO: use this.Content to load your game content here
         }
@@ -130,8 +193,11 @@ namespace Primitiva3D
 
             efeitoBasico.CurrentTechnique.Passes[0].Apply();
 
-            GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip,
-                primitivas, 0, 1);
+            efeitoBasico.World = worldTest;
+
+            //não esquecer de mudar para list e a quantidade de primitivas desenhadas...
+            GraphicsDevice.DrawUserPrimitives<VertexPositionColorTexture>(PrimitiveType.TriangleList,
+                primitivas, 0, 2);
 
             // TODO: Add your drawing code here
 
